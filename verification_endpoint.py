c6844993bad9e26e4d5c7fd42ebd5f4c36e9b7b0
charros96 <88594194+charros96@users.noreply.github.com>
2022-03-21 16:10:44 -0700

    Add files via upload

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,7 +11,7 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    content = json.loads(content)
+    #content = json.loads(content)
     sig = content["sig"]
     payload = content["payload"]
     platform = payload["platform"]
@@ -23,7 +23,7 @@ def verify():
     elif platform == "Algorand":
         pass
     #Check if signature is valid
-    #result = True #Should only be true if signature validates
+    result = True #Should only be true if signature validates
     return jsonify(result)
 
 if __name__ == '__main__':
