94337623a2de2c7c690b58b592e60f7fad73d28d
charros96 <88594194+charros96@users.noreply.github.com>
2022-03-21 16:39:24 -0700

    Add files via upload

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,7 +11,7 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    #print(content)
+    print(type(content))
     #y = json.loads(content)
     #sig = y["sig"]
     #payload = y["payload"]
